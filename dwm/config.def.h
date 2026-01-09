258d8cd903974b80d1abc708d5d90f8449a1a59f
stjmm <shetrynajerkme@gmail.com>
2026-01-09 17:09:29 +0100

    other changes

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "FiraCode Nerd Font:size=16" };
+static const char dmenufont[]       = "FiraCode Nerd Font:size=16";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -63,6 +63,8 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
+#include <X11/XF86keysym.h>
+
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
@@ -98,6 +100,12 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+
+	{0,XF86XK_AudioRaiseVolume,spawn,SHCMD("pactl set-sink-volume @DEFAULT_SINK@ +5%")},
+	{0,XF86XK_AudioLowerVolume,spawn,SHCMD("pactl set-sink-volume @DEFAULT_SINK@ -5%")},
+	{0,XF86XK_AudioMute,spawn,SHCMD("pactl set-sink-mute @DEFAULT_SINK@ toggle")},
+	{0,XF86XK_MonBrightnessUp,spawn,SHCMD("brightnessctl set +10%")},
+	{0,XF86XK_MonBrightnessDown,spawn,SHCMD("brightnessctl set 10%-")},
 };
 
 /* button definitions */
