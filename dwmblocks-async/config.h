69ac13731996852349b16149963eb8b2849a2cc6
stjmm <shetrynajerkme@gmail.com>
2026-01-10 16:19:49 +0100

    [dwmblocks] some scripts

--- dwmblocks-async/config.h
+++ dwmblocks-async/config.h
@@ -2,7 +2,7 @@
 #define CONFIG_H
 
 // String used to delimit block outputs in the status.
-#define DELIMITER "  "
+#define DELIMITER " | "
 
 // Maximum number of Unicode characters that a block can output.
 #define MAX_BLOCK_OUTPUT_LENGTH 45
